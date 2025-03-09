055571bfd0edacb4c4bc2f2516b74c6833a0821e
James Crosswell <jamescrosswell@users.noreply.github.com>
2025-03-10 12:09:01 +1300

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace MauiWipeout;
+
+public partial class MainPage : ContentPage
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+	// TODO: This needs to be wired up to a real authentication system
+	const string _loginUrl = "https://localhost:5001/api/auth/login";
+
+    public MainPage(IHttpClientFactory httpClientFactory)
+	{
+        _httpClientFactory = httpClientFactory;
+    	InitializeComponent();
+	}
+
+	private async void OnLoginButtonClicked(object sender, EventArgs e)
+	{
+		var loginData = new LoginRequest(UsernameEntry.Text, PasswordEntry.Text);
+		var json = JsonSerializer.Serialize(loginData);
+
+		using var client = _httpClientFactory.CreateClient();
+		// TODO: We should really use async here, and maybe use a third party for authentication
+		var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Post, _loginUrl)
+		{
+			Content = new StringContent(json, Encoding.UTF8, "application/json")
+		});
+		if (response.IsSuccessStatusCode)
+		{
+			HandleSuccessfulLogin();
+		}
+		else
+        {
+            await DisplayAlert("Login Failed", "Invalid username or password. Please try again.", "OK");
+        }
+	}
+
+    record LoginRequest(string Username, string Password);
+
+    private void HandleSuccessfulLogin()
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ MauiProgram.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiWipeout;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.UseSentry(options =>
+			{
+				options.Dsn = "https://eb18e953812b41c3aeb042e666fd3b5c@o447951.ingest.sentry.io/5428537";
+				options.Debug = true;
+			})
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			})
+			.Services.AddSingleton<IHttpClientFactory, SentryHttpClientFactory>();
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+
+	class SentryHttpClientFactory : IHttpClientFactory
+	{
+		public HttpClient CreateClient(string name) => new HttpClient(new SentryHttpMessageHandler());
+	}
+}
